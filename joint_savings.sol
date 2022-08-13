972a422120d62995974c641fcfb43f67196fe754
ghedrick76 <graysonh76@icloud.com>
2022-08-13 11:30:29 -0400

    touched up code

--- joint_savings.sol
+++ joint_savings.sol
@@ -1,42 +1,15 @@
-/*
-Joint Savings Account
----------------------
-
-To automate the creation of joint savings accounts, you will create a solidity smart contract that accepts two user addresses that are then able to control a joint savings account. Your smart contract will use ether management functions to implement various requirements from the financial institution to provide the features of the joint savings account.
-
-The Starting file provided for this challenge contains a `pragma` for solidity version `5.0.0`.
-You will do the following:
-
-1. Create and work within a local blockchain development environment using the JavaScript VM provided by the Remix IDE.
-
-2. Script and deploy a **JointSavings** smart contract.
-
-3. Interact with your deployed smart contract to transfer and withdraw funds.
-
-*/
-
 pragma solidity ^0.5.0;
 
 // Define a new contract named `JointSavings`
 contract JointSavings {
 
-    /*
-    Inside the new contract define the following variables:
-    - Two variables of type `address payable` named `accountOne` and `accountTwo`
-    - A variable of type `address public` named `lastToWithdraw`
-    - Two variables of type `uint public` named `lastWithdrawAmount` and `contractBalance`.
-    */
     address payable accountOne;
     address payable accountTwo;
     address public lastToWithdraw;
     uint public lastWithdrawAmount;
     uint public contractBalance;
 
-    /*
-    Define a function named **withdraw** that will accept two arguments.
-    - A `uint` variable named `amount`
-    - A `payable address` named `recipient`
-    */
+    //Function to allow accounts to withdraw from the smart contract
     function withdraw(uint amount, address payable recipient) public {
 
         require(recipient == accountOne || recipient == accountTwo, "You don't own this account!");
@@ -49,21 +22,13 @@ contract JointSavings {
                 contractBalance = address(this).balance;
         }
     }
-    // Define a `public payable` function named `deposit`.
-    function deposit() public payable {
-
-        /*
-        Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
-        */
+    //Function to deposit
         contractBalance = address(this).balance;
     }
 
-    /*
-    Define a `public` function named `setAccounts` that receive two `address payable` arguments named `account1` and `account2`.
-    */
+    
+    //Function to choose which to choose which accounts will be affected.
     function setAccounts(address payable account1, address payable account2) public{
-
-        // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
         accountOne = account1;
         accountTwo = account2;
     }
